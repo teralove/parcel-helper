b1bade1e98ae608c39e64d51e0db424a48e6f05b
teralove <terascripts@gmail.com>
2018-03-24 09:28:53 -0400

    1.2 update

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-# Parcel Helper
+# parcel-helper
+tera-proxy module that quickly manages a ton of parcels and old messages in your mailbox.
 
-Quick way to manage a ton of parcels and old messages in your mailbox.
+## Dependency
+- `Command` module
 
-## Features:
-* Instantly accepts all parcels on every page.
-* Instantly deletes every read message on all pages.
+## Usage
+### `getmail`
+- Accepts all parcels
+### `deletemail` 'delmail'
+- Deletes all read messages
 
+## Changelog
+<details>
 
-## Chat Commands
-* **!GetMail** - Claim parcels
-* **!DeleteMail** or **!DelMail** - Delete read messages
+    1.20
+    - Code aesthetics
+    - Updated for guardian patch
+    - Added command dependency
+    - Removed slash support
+    1.10
+    - Added [slash](https://github.com/baldera-mods/slash) support
+    
+</details>
 
-Commands are not case-sensitive, they can be all lower-case (!getmail), upper (!GETMAIL), whatever (!gEtMaiL)
-
-
-## Known Issues
-* Claiming parcels when the mailbox is open might cause the procedure to halt mid-way. Closing the mailbox will trigger the procedure to continue. Workaround is to make sure the mailbox window is closed before calling !getmail.
-
-
-## Changelog 
-### 1.1.0
-* [+] Added [slash](https://github.com/baldera-mods/slash) support
-
+---
--- index.js
+++ index.js
@@ -1,214 +1,200 @@
-/* vers 1.1 */
-
-const format = require('./format.js');
+/*
+OPCodes Required
+- C_LIST_PARCEL
+- S_LIST_PARCEL_EX
+- C_DELETE_PARCEL
+- C_REQUEST_CONTRACT
+- S_REPLY_REQUEST_CONTRACT
+- C_SHOW_PARCEL_MESSAGE
+- S_PARCEL_READ_RECV_STATUS
+- C_RECV_PARCEL
+- S_RECV_PARCEL
+*/
+const Command = require('command');
 
 module.exports = function ParcelHelper(dispatch) {
+        
+    const command = Command(dispatch);
+    
+    /* 
+        States 
+    */
+    const Idle = 0;
+    const PreparingDeletion = 1;
+    const PreparingParcels = 2;
+    const ClaimingParcels = 3;
 
-	/* 
-		States 
-	*/
-	const Idle = 0;
-	const PreparingDeletion = 1;
-	const PreparingParcels = 2;
-	const ClaimingParcels = 3;
+    let state = Idle;
+    let messageIds = [];
 
-	let state = Idle;
-	let messageIds = [];
-
-	/* 
-		Chat hooks
-	*/
-    const chatHook = event => {		
-		let command = format.stripTags(event.message).split(' ');
-		
-		if (['!getmail'].includes(command[0].toLowerCase())) {
-			startProcedure(PreparingParcels);
-			return false;
-		} 
-		else if (['!deletemail', '!delmail'].includes(command[0].toLowerCase())) {
-			startProcedure(PreparingDeletion)
-			return false;
-		}
+    /* 
+        Chat hooks
+    */
+    command.add('getmail', (arg1) => {
+        startProcedure(PreparingParcels);
+    });
+    
+    command.add('deletemail', (arg1) => {
+        startProcedure(PreparingDeletion)
+    });
+    
+    command.add('delmail', (arg1) => {
+        startProcedure(PreparingDeletion)
+    });
+    
+    /*
+        Populate messageIds array 
+        
+        Dispatching C_LIST_PARCEL will trigger the server to send S_LIST_PARCEL_EX, with this we can get analyze every message
+        in the inbox and store the corresponding message id.
+    */
+    
+    function startProcedure(startState) {
+        messageIds = [];
+        state = startState;
+        checkMail(0);
     }
-    dispatch.hook('C_CHAT', 1, chatHook)
-    dispatch.hook('C_WHISPER', 1, chatHook)
-	
-	// slash support, thanks to wuav for snippet
-	try {
-		const Slash = require('slash')
-		const slash = new Slash(dispatch)
-		slash.on('getmail', args => startProcedure(PreparingParcels))
-		slash.on('deletemail', args => startProcedure(PreparingDeletion))
-		slash.on('delmail', args => startProcedure(PreparingDeletion))
-	} catch (e) {
-		// do nothing because slash is optional
-	}
-	
-    function systemMsg(msg) {
-        dispatch.toClient('S_CHAT', 1, {
-            channel: 24,
-            authorID: 0,
+    
+    function checkMail(pageIndex) {
+        dispatch.toServer('C_LIST_PARCEL', 2, {
             unk1: 0,
-            gm: 0,
-            unk2: 0,
-            authorName: '',
-            message: ' (parcel-helper) ' + msg
+            page: pageIndex,
+            filter: 0
         });
     }
-
-	/*
-		Populate messageIds array 
-		
-		Dispatching C_LIST_PARCEL will trigger the server to send S_LIST_PARCEL_EX, with this we can get analyze every message
-		in the inbox and store the corresponding message id.
-	*/
-	
-	function startProcedure(startState) {
-		messageIds = [];
-		state = startState;
-		checkMail(0);
-	}
-	
-	function checkMail(pageIndex) {
-		dispatch.toServer('C_LIST_PARCEL', 2, {
-			unk1: 0,
-			page: pageIndex,
-			filter: 0
-		});
-	}
-	
-    dispatch.hook('S_LIST_PARCEL_EX', 1, (event) => {		
-		switch (state) {
-			case PreparingDeletion:
-				for (let i in event.messages) {				
-					// only accept id's of read messages and claimed parcels
-					if (event.messages[i].read == 2) {
-						 messageIds.push({id: event.messages[i].id});
-					}
-				}	
-
-				// check next page
-				if (event.currentPage < event.totalPages - 1) {
-					checkMail(event.currentPage + 1);
-				}
-				else {
-					// no more pages to check, try deleting now
-					if (messageIds.length > 0) {
-						dispatch.toServer('C_DELETE_PARCEL', 1, {
-							unk: 0,
-							messages: messageIds
-						});
-					}
-					else {
-						systemMsg('No messages to delete');
-					}
-					state = Idle;
-					
-				}				
-				break;
-				
-			case PreparingParcels:
-				for (let i in event.messages) {				
-					// only accept id's of unclaimed parcels
-					if (event.messages[i].type != 0 && event.messages[i].read != 2) {
-						 messageIds.push({id: event.messages[i].id});
-					}
-				}
-				
-				// check next page
-				if (event.currentPage < event.totalPages - 1) {
-					checkMail(event.currentPage + 1);
-				}
-				else {
-					if (messageIds.length > 0) {
-						state = ClaimingParcels;
-						requestContract();
-					}
-					else {
-						systemMsg('No parcels to claim');
-						state = Idle;
-					}
-				}						
-				break;
-		};
-						
+    
+    dispatch.hook('S_LIST_PARCEL_EX', 1, (event) => {
+        
+        switch (state) {
+            case PreparingDeletion:
+                for (let i in event.messages) {
+                    // only accept id's of read messages and claimed parcels
+                    if (event.messages[i].read == 2) {
+                        messageIds.push({id: event.messages[i].id});
+                    }
+                }	
+                
+                // check next page
+                if (event.currentPage < event.totalPages - 1) {
+                    checkMail(event.currentPage + 1);
+                }
+                else {
+                    // no more pages to check, try deleting now
+                    if (messageIds.length > 0) {
+                        dispatch.toServer('C_DELETE_PARCEL', 2, {
+                            messages: messageIds
+                        });
+                    }
+                    else {
+                        command.message('(parcel-helper) No messages to delete');
+                    }
+                    state = Idle;
+                
+                }
+                break;
+            
+            case PreparingParcels:
+                for (let i in event.messages) {
+                    // only accept id's of unclaimed parcels
+                    if (event.messages[i].type != 0 && event.messages[i].read != 2) {
+                        messageIds.push({id: event.messages[i].id});
+                    }
+                }
+                
+                // check next page
+                if (event.currentPage < event.totalPages - 1) {
+                    checkMail(event.currentPage + 1);
+                }
+                else {
+                    if (messageIds.length > 0) {
+                        state = ClaimingParcels;
+                        requestContract();
+                    }
+                    else {
+                        command.message('(parcel-helper) No parcels to claim');
+                        state = Idle;
+                    }
+                }
+                break;
+        };
+    
     });
 
-	/*		
-		Parcels need to request a contract once and then all need to be read before being claimed.
-		
-		Start:
-			C_REQUEST_CONTRACT
-			S_REPLY_REQUEST_CONTRACT (or S_REQUEST_CONTRACT ?)
-		Loop:			
-			C_SHOW_PARCEL_MESSAGE
-			S_PARCEL_READ_RECV_STATUS ?
-			C_RECV_PARCEL
-			S_RECV_PARCEL
-	*/
-		
-	function requestContract() {
-		dispatch.toServer('C_REQUEST_CONTRACT', 1, {
-			type: 8
-		});		
-	}
-	
+    /*  
+        Parcels need to request a contract once and then all need to be read before being claimed.
+        
+        Start:
+            C_REQUEST_CONTRACT
+            S_REPLY_REQUEST_CONTRACT (or S_REQUEST_CONTRACT ?)
+        Loop:
+            C_SHOW_PARCEL_MESSAGE
+            S_PARCEL_READ_RECV_STATUS ?
+            C_RECV_PARCEL
+            S_RECV_PARCEL
+    */
+        
+    function requestContract() {
+        dispatch.toServer('C_REQUEST_CONTRACT', 1, {
+            type: 8
+        });
+    }
+    
     dispatch.hook('S_REPLY_REQUEST_CONTRACT', 1, (event) => {
-		if (state == ClaimingParcels) {
-			readParcel();
-		}		
-	});
-	
-	/*
-		Parcel claim loop
-		
-		TODO: Find a better hook than S_PARCEL_READ_RECV_STATUS ??? What happens is it gets triggere twice, once from C_SHOW_PARCEL_MESSAGE and 
-		again from S_RECV_PARCEL. So what happens is the S_PARCEL_READ_RECV_STATUS hook funciton will call claimParcel twice on the same parcel.
-		This will cause every parcel to be claimed and then immediately fail, do this enough times and the client will disconnect and possibly crash. 
-		The current solution is keep track of the parcel id that is currently being processed and ignore the second S_PARCEL_READ_RECV_STATUS execution,
-		this is done with the queuedParcel variable.
-	*/
-	let queuedParcel = 0;
-	
-	function readParcel() {
-		if (messageIds.length > 0) {
-			queuedParcel = messageIds[messageIds.length-1].id;
-			dispatch.toServer('C_SHOW_PARCEL_MESSAGE', 1, {
-				id: messageIds[messageIds.length-1].id
-			});		
-		}
-		else {
-			state = Idle;
-		}
-	}
+        if (state == ClaimingParcels) {
+            readParcel();
+        }
+    });
+    
+    /*
+        Parcel claim loop
+        
+        TODO: Find a better hook than S_PARCEL_READ_RECV_STATUS ??? What happens is it gets triggere twice, once from C_SHOW_PARCEL_MESSAGE and 
+        again from S_RECV_PARCEL. So what happens is the S_PARCEL_READ_RECV_STATUS hook funciton will call claimParcel twice on the same parcel.
+        This will cause every parcel to be claimed and then immediately fail, do this enough times and the client will disconnect and possibly crash. 
+        The current solution is keep track of the parcel id that is currently being processed and ignore the second S_PARCEL_READ_RECV_STATUS execution,
+        this is done with the queuedParcel variable.
+    */
+    let queuedParcel = 0;
+    
+    function readParcel() {
+        if (messageIds.length > 0) {
+            queuedParcel = messageIds[messageIds.length-1].id;
+            dispatch.toServer('C_SHOW_PARCEL_MESSAGE', 1, {
+                id: messageIds[messageIds.length-1].id
+            });
+        }
+        else {
+            state = Idle;
+        }
+    }
 
     dispatch.hook('S_PARCEL_READ_RECV_STATUS', (event) => {
-		if (state == ClaimingParcels ) {
-			if (queuedParcel != 0 && queuedParcel == messageIds[messageIds.length-1].id) {
-				queuedParcel = 0;
-				claimParcel();
-			}
-		}		
-	});
-	
-	function claimParcel() {
-		if (messageIds.length > 0) {
-			dispatch.toServer('C_RECV_PARCEL', 1, {
-				id: messageIds[messageIds.length-1].id
-			});
-		}
-	}
-	
-	dispatch.hook('S_RECV_PARCEL', 2, (event) => {
-		if (state == ClaimingParcels) {
-			if (messageIds.length > 0) {
-				messageIds.pop();
-				readParcel();
-			}
-			else {
-				state = Idle;
-			}
-		}
-	});
+        if (state == ClaimingParcels ) {
+            if (queuedParcel != 0 && queuedParcel == messageIds[messageIds.length-1].id) {
+                queuedParcel = 0;
+                claimParcel();
+            }
+        }
+    });
+    
+    function claimParcel() {
+        if (messageIds.length > 0) {
+            dispatch.toServer('C_RECV_PARCEL', 1, {
+                id: messageIds[messageIds.length-1].id
+            });
+        }
+    }
+    
+    dispatch.hook('S_RECV_PARCEL', 2, (event) => {
+        if (state == ClaimingParcels) {
+            if (messageIds.length > 0) {
+                messageIds.pop();
+                readParcel();
+            }
+            else {
+                state = Idle;
+            }
+        }
+    });
 
 }
